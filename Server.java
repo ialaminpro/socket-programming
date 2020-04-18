d44a5f820ac621d3900b748d425e52955ed643ee
Al-Amin <ialamin.pro@gmail.com>
2020-04-18 10:24:52 +0600

    Repeat operation

--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 // A Java program for a Server 
 import java.net.*; 
 import java.io.*; 
+import java.math.*;
 
 public class Server 
 { 
@@ -8,6 +9,15 @@ public class Server
 	private Socket		 socket = null; 
 	private ServerSocket server = null; 
 	private DataInputStream in	 = null; 
+
+	public static boolean isNumber(String num){
+		try {  
+	    	Double.parseDouble(num);  
+	    	return true;
+	  	} catch(NumberFormatException e){  
+	    	return false;  
+	  	}  
+	}
 
 	// constructor with port 
 	public Server(int port) 
@@ -28,9 +38,8 @@ public class Server
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 
 			String str=""; 
-			int firstNumber = 0;
-			int secondNumber = 0;;
-			int result = 0;
+			String firstNumber = "";
+			String secondNumber = "";
 			String opt = "";
 			int flag = 0;
 			while(!str.equals("stop")){  
@@ -44,34 +53,86 @@ public class Server
 						dout.flush(); 
 					}else{
 						flag = 0;
-						dout.writeUTF("Enter arithmetic operator: ");  
+						dout.writeUTF("Unknown operation");  
 						dout.flush(); 
 					}
 				}
 				else if(flag == 1){
-					firstNumber = Integer.parseInt(str.trim());
-					dout.writeUTF("Enter Second Number: ");  
-					dout.flush();
-					flag++;
-				}else{
-					secondNumber = Integer.parseInt(str.trim());
-					
-					if(opt.equals("+")){
-						result = firstNumber + secondNumber;
-					}else if(opt.equals("-")){
-						result = firstNumber - secondNumber;
-					}else if(opt.equals("*")){
-						result = firstNumber * secondNumber;
-					}else if(opt.equals("/")){
-						result = firstNumber / secondNumber;
+					firstNumber = str;
+					// firstNumber = Integer.parseInt(str.trim());
+					if(isNumber(firstNumber)==false){
+						dout.writeUTF("First value: not a number");  
+						dout.flush();
+						flag++;
+					}else{
+						
+						dout.writeUTF("Enter Second Number: ");  
+						dout.flush();
+						flag++;
 					}
-					 
-					flag = 0;
-					dout.writeUTF("Result = "+result);  
-					dout.flush();  	
+					
+				}else{
+					secondNumber = str;
+					// secondNumber = Integer.parseInt(str.trim());
+					if(isNumber(firstNumber)==false && isNumber(secondNumber) == false){
+						dout.writeUTF("Both values: not numbers");  
+						dout.writeUTF("Enter First Number: ");
+						dout.flush();
+						flag = 1;
+					}
+					else if(isNumber(secondNumber) == false){
+						dout.writeUTF("Second value: not a number");  
+						dout.flush();
+						flag++;
+					}else{
+						
+						flag++;
+						if(opt.equals("+")){
+							BigInteger i1 = new BigInteger(firstNumber);
+							BigInteger i2 = new BigInteger(secondNumber);
+							BigInteger result;
+							result = i1.add(i2);
+							flag = 0;
+							dout.writeUTF("The sum is  "+result);  
+							dout.flush();
+
+						}else if(opt.equals("-")){
+							BigInteger i1 = new BigInteger(firstNumber);
+							BigInteger i2 = new BigInteger(secondNumber);
+							BigInteger result;
+							result = i1.subtract(i2);
+							flag = 0;
+							dout.writeUTF("The subtract is  "+result);  
+							dout.flush();
+						}else if(opt.equals("*")){
+							BigInteger i1 = new BigInteger(firstNumber);
+							BigInteger i2 = new BigInteger(secondNumber);
+							BigInteger result;
+							result = i1.multiply(i2);
+							flag = 0;
+							dout.writeUTF("The multiply is  "+result);  
+							dout.flush();
+						}else if(opt.equals("/")){
+							if(secondNumber.equals("0")){
+								dout.writeUTF("Divide by 0");  
+								dout.flush();
+							}else{
+								BigInteger i1 = new BigInteger(firstNumber);
+								BigInteger i2 = new BigInteger(secondNumber);
+								BigInteger result;
+								result = i1.divide(i2);
+								flag = 0;
+								dout.writeUTF("The divide is  "+result);  
+								dout.flush();
+							}
+							
+						}
+						 
+						  
+					}	
 				}
 				
-				System.out.println(result);  
+				// System.out.println(result);  
 					// System.out.println("client says: "+input);  
 					// str2=br.readLine();  
 					// String res = Integer.toString(result);
