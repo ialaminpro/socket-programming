c6c80a53e3601925f0dd3c48df655061e75f8bae
Al-Amin <ialamin.pro@gmail.com>
2020-04-19 20:56:20 +0600

    Update Server and Client

--- Client.java
+++ Client.java
@@ -6,8 +6,9 @@ public class Client
 { 
 	// initialize socket and input output streams 
 	private Socket socket		 = null; 
-	private DataInputStream input = null; 
-	private DataOutputStream out	 = null; 
+	private BufferedReader input = null; 
+	private DataOutputStream out = null; 
+	private DataInputStream din = null;
 
 	// constructor to put ip address and port 
 	public Client(String address, int port) 
@@ -17,44 +18,59 @@ public class Client
 		{ 
 			socket = new Socket(address, port); 
 			System.out.println("Connected"); 
-			System.out.println("Enter arithmetic operator"); 
+			 
+			out = new DataOutputStream(socket.getOutputStream());  
+			input = new BufferedReader(new InputStreamReader(System.in));
+			din = new DataInputStream(socket.getInputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
 
-			DataInputStream din=new DataInputStream(socket.getInputStream());  
-			DataOutputStream dout=new DataOutputStream(socket.getOutputStream());  
-			
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			String str="",str2=""; 
-			int input;
+		System.out.println("Enter arithmetic operator"); 
+		String line = "";
+		String line2 = "";
 
-			while(!str.equals("stop")){  
-				// input=br.readLine();  
-				str=br.readLine(); 
-				// input = Integer.parseInt(str.trim()); 
-				dout.writeUTF(str);  
-				dout.flush();  
-				str2=din.readUTF();  
-				System.out.println(str2);  
-				// System.out.println("Server says: "+str2);  
+		while(!line.equals("stop")){  
+			try
+			{
+				line = input.readLine(); 
+				out.writeUTF(line);  
+				out.flush(); 
+				line2 = din.readUTF(); 
+				System.out.println(line2); 
+				if(line2.contains("The") == true){
+					System.out.println("Disconnected!");
+				}
+				 
+
+			}
+			catch(IOException i)
+			{
+				System.out.println(i);
 			}  
-			  
-			dout.close();  
+		}
+		// close the connection
+		try
+		{
+			input.close();
+			out.close();
 			socket.close();
-
-			
-		} 
-		catch(UnknownHostException u) 
-		{ 
-			System.out.println(u); 
-		} 
-		catch(IOException i) 
-		{ 
-			System.out.println(i); 
-		} 
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
 	} 
 
 	public static void main(String args[]) 
 	{ 
-		Client client = new Client("127.0.0.1", 5005); 
+		Client client = new Client("127.0.0.1", 5031); 
 	} 
 } 
 
--- Server.java
+++ Server.java
@@ -8,7 +8,9 @@ public class Server
 	//initialize socket and input stream 
 	private Socket		 socket = null; 
 	private ServerSocket server = null; 
-	private DataInputStream in	 = null; 
+	private DataInputStream din	 = null;
+	private DataOutputStream dout = null;
+	private DataInputStream input = null; 
 
 	public static boolean isNumber(String num){
 		try {  
@@ -33,18 +35,35 @@ public class Server
 			socket = server.accept(); 
 			System.out.println("Client accepted"); 
 
-			DataInputStream din = new DataInputStream(socket.getInputStream());  
-			DataOutputStream dout = new DataOutputStream(socket.getOutputStream());  
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+			din = new DataInputStream(socket.getInputStream());  
+			dout = new DataOutputStream(socket.getOutputStream()); 
+			input = new DataInputStream(new BufferedInputStream(socket.getInputStream())); 
 
-			String str=""; 
-			String firstNumber = "";
-			String secondNumber = "";
-			String opt = "";
-			int flag = 0;
-			while(!str.equals("stop")){  
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
 
-				str=din.readUTF(); 
+
+		String str = ""; 
+		String firstNumber = "";
+		String secondNumber = "";
+		String opt = "";
+		int flag = 0;
+		String line = "";
+		while(socket.isConnected()){  
+		// while(!str.equals("Stop")){  
+			try
+			{
+				// line = input.readUTF();
+				// System.out.println(line);
+				str = input.readUTF(); 
+				System.out.println(str);
 				if(flag == 0){
 					if(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")){
 						opt = str;
@@ -95,7 +114,6 @@ public class Server
 							flag = 0;
 							dout.writeUTF("The sum is  "+result);  
 							dout.flush();
-
 						}else if(opt.equals("-")){
 							BigInteger i1 = new BigInteger(firstNumber);
 							BigInteger i2 = new BigInteger(secondNumber);
@@ -131,27 +149,33 @@ public class Server
 						  
 					}	
 				}
+			}
+			catch(IOException i)
+			{
+				System.out.println("Closing connection");
 				
-				// System.out.println(result);  
-					// System.out.println("client says: "+input);  
-					// str2=br.readLine();  
-					// String res = Integer.toString(result);
-					// dout.writeUTF("Result = ");  
-					// dout.writeUTF(res);  
-					// dout.flush(); 				 
-			} 
-		din.close();  
-		socket.close();  
-		server.close(); 
+				System.out.println(i);
+			}				 
 		} 
-		catch(IOException i) 
-		{ 
-			System.out.println(i); 
-		} 
+		// close the connection
+		try
+		{
+			System.out.println("Closing connection");
+			input.close(); 
+			din.close(); 
+			dout.close(); 
+			socket.close();
+			server.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+			
 	} 
 
 	public static void main(String args[]) 
 	{ 
-		Server server = new Server(5005); 
+		Server server = new Server(5031); 
 	} 
 } 
